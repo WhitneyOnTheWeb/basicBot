32010dc46dc8c11281967838d7029cf4502707d8
Whitney <whitney@whitneyontheweb.com>
2015-07-03 13:00:52 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -46,7 +46,7 @@
                 else {
                     //API.sendChat(subChat(basicBot.chat.bacon, {name: chat.un}));
                     //API.sendChat("/me http://big.assets.huffingtonpost.com/slide_297900_2459978_free.gif");
-                    API.sendChat("/me @" + chat.un + ",http://big.assets.huffingtonpost.com/slide_297900_2459978_free.gif");
+                    API.sendChat("/me [@" + chat.un + "]http://big.assets.huffingtonpost.com/slide_297900_2459978_free.gif");
                 }
             }
         };
