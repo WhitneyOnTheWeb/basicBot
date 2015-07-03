9f4f0adacbb22e2034ca4153c816a8751f7ac95f
Whitney <whitney@whitneyontheweb.com>
2015-07-02 23:26:11 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -51,9 +51,24 @@
             }
         };
         
+        // !blazing
+        bot.commands.blazingCommand = {
+            command: 'blazing',  //The command to be called. With the standard command literal this would be: !blazing
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    //API.sendChat(subChat(basicBot.chat.blazing, {name: chat.un}));
+                    API.sendChat("/me http://i.imgur.com/tcnm9.gif");
+                }
+            }
+        };
+        
         // !derp
         bot.commands.derpCommand = {
-            command: 'derp',  //The command to be called. With the standard command literal this would be: !bacon
+            command: 'derp',  //The command to be called. With the standard command literal this would be: !derp
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
