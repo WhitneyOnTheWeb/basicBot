41c328cee475a9a386ebf5236820ac3949f44ab2
Whitney <whitney@whitneyontheweb.com>
2015-07-10 21:15:38 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -215,21 +215,21 @@
         };
         
         // !passesit
-        //bot.commands.passesitCommand = {
-        //    command: ['passit', 'passesit'],  //The command to be called. With the standard command literal this would be: !passesit
-        //    rank: 'user', //Minimum user permission to use the command
-        //    type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-        //    functionality: function (chat, cmd) {
-        //        if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-        //        if (!bot.commands.executable(this.rank, chat)) return void (0);
-        //        else {
-        //            API.sendChat("/me @" + chat.un + " takes one quick puff and then passes it to the left :herb: :smoking:");
-        //        }
-        //    }
-        //};
+        bot.commands.passesitCommand = {
+            command: ['passesit'],  //The command to be called. With the standard command literal this would be: !passesit
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me @" + chat.un + " takes one quick puff and then passes it to the left :herb: :smoking:");
+                }
+            }
+        };
 		
-		bot.commands.passesitCommand = { 
-            command: ['passit', 'passesit'],  //The command to be called. With the standard command literal this would be: !passesit
+		bot.commands.passitaroundCommand = { 
+            command: ['passit'],  //The command to be called. With the standard command literal this would be: !passesit
             rank: 'user', 
             type: 'startsWith',  
             functionality: function (chat, cmd) { 
@@ -249,7 +249,7 @@
                                  return API.sendChat("/me takes one quick puff and looks around for @" + name + ", but doesn't see them :herb: :smoking:"); 
                              } 
                              else if (user.username === chat.un) { 
-                                 return API.sendChat("/me sparks one up and burns it all the way to the ground. :herb: :smoking: Share next time!"); 
+                                 return API.sendChat("/me @" + chat.un + " sparks one up and burns it all the way to the ground. :herb: :smoking: Share next time!"); 
                              } 
                              else { 
                                  return API.sendChat("/me takes one quick puff and passes it to @" + name + " :herb: :smoking:, keep it going!"); 
