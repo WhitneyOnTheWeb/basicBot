51ae23dcbe5e8c209384982ef00ce71ca1f86473
Whitney <whitney@whitneyontheweb.com>
2015-07-10 23:14:35 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -57,10 +57,6 @@
             command: ['blaze', 'blazing'],  //The command to be called. With the standard command literal this would be: !blazing
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-            getPot: function (chat) {
-               var c = Math.floor(Math.random() * bot.chat.blazing.length);
-               return bot.chat.blazing[c];
-                },
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
