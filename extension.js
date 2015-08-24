ba368e0c7704b52364751660a9f6969a86dad799
Whitney <whitney@whitneyontheweb.com>
2015-08-24 12:11:43 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -545,14 +545,14 @@
                              } 
                          else if (space === -1) { 
                             //Start Slots
-                            API.sendChat("/me @" + chat.un + " bets one TOKEn at the ChemSlots, and pulls the handle to spin... " + chat.un + " watches the ChemSlots spin. It finally stops on: ");
-                            setTimeout(function() {API.sendChat("/me " + outcome[0] + outcome[1] + outcome[2])}, 1000);
+                            API.sendChat("/me @" + chat.un + " bets one TOKEn at the ChemSlots, and pulls the handle to spin... " + chat.un + " watches the ChemSlots spin.");
+                            setTimeout(function() {API.sendChat("/me  It finally stops on: " + outcome[0] + outcome[1] + outcome[2])}, 5000);
                             setTimeout(function() {API.sendChat("\"" + bet + "\"")}, 3000);
                          } 
                          else if (bet >= 1) { 
                             //Start Slots
-                            API.sendChat("/me @" + chat.un + " bets " + bet + " TOKEns at the ChemSlots, and pulls the handle to spin... " + chat.un + " watches the ChemSlots spin. It finally stops on: ");
-                            setTimeout(function() {API.sendChat("/me " + outcome[0] + outcome[1] + outcome[2])}, 1000);
+                            API.sendChat("/me @" + chat.un + " bets " + bet + " TOKEns at the ChemSlots, and pulls the handle to spin... " + chat.un + " watches the ChemSlots spin.");
+                            setTimeout(function() {API.sendChat("/me It finally stops on: " + outcome[0] + outcome[1] + outcome[2])}, 5000);
                             setTimeout(function() {API.sendChat("\"" + bet + "\"")}, 3000);
                          } 
                          else {
