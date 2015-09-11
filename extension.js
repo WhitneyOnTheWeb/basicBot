bffbbde4930f3b10067ce90df349cda219f5f572
Whitney <whitney@whitneyontheweb.com>
2015-09-10 21:50:52 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -451,6 +451,23 @@
             }
         };
         
+        
+        // !cleartokens
+        bot.commands.cleartokensCommand = {
+            command: 'cleartokens',  //The command to be called. With the standard command literal this would be: !cleartokens
+            rank: 'manager', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    localStorage.clear();
+                }
+            }
+        };
+        
+        
+        //Slots---------------------------------------------------------------------------------------------------------------------------
         function spinSlots() {
             var slotArray = [':lemon:',
                              ':tangerine:', 
